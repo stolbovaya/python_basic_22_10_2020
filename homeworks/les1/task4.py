a7eece83338d34d1f009935c0692d07d3e5fb990
stolbovaya <stolbovaya@yahoo.com>
2020-10-26 22:38:09 +0300

    optimization of task 4.

--- homeworks/les1/task4.py
+++ homeworks/les1/task4.py
@@ -3,15 +3,22 @@
 Для решения используйте цикл while и арифметические операции.
 """
 
-user_str = (input('введите целое положительное число\n >>>'))
-idx = len(user_str)-1
-user_int = int(user_str)
+user_int = int(input('введите целое положительное число\n >>>'))
+count = 0
+user_count = user_int
+while user_count:
+    user_count //= 10
+    count += 1
+
+idx = count - 1
 max_int = -1
-while idx >= 0:
+
+while idx >= 0 and max_int != 9:
     cur_int = user_int // 10**idx
     if cur_int > max_int:
         max_int = cur_int
-    user_int = user_int % 10**idx
+    user_int %= 10**idx
     idx -= 1
+
 print(f"максимальная цифра = {max_int}")
 
