ce237259fccc2c375f5d6bdce1a8f7771b0dd497
stolbovaya <stolbovaya@yahoo.com>
2020-10-26 22:21:55 +0300

    fixed task 3. A number can contain more than one digit

--- homeworks/les1/task3.py
+++ homeworks/les1/task3.py
@@ -7,7 +7,14 @@ user_int = int(input('введите целое число\n >>>'))
 result = 0
 idx = 0
 idx_max = 3
+count = 0
+user_count = user_int
+
+while user_count:
+    user_count //= 10
+    count += 1
+
 while idx < idx_max:
-    result += (idx_max-idx)*10**idx*user_int
+    result += (idx_max-idx)*10**(idx*count)*user_int
     idx += 1
 print(f"сумма чисел = {result}")
