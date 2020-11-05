c94258791a97dd2b7555bffd4ae9f72333293768
stolbovaya <stolbovaya@yahoo.com>
2020-11-05 19:57:03 +0300

    Task 2

---
+++ homeworks/less4/task2.py
@@ -0,0 +1,14 @@
+"""
+2. Представлен список чисел. Необходимо вывести элементы исходного списка, значения которых больше предыдущего элемента.
+"""
+import random
+
+user_list = [el for el in range(random.randint(0, 1000))]
+print(user_list)
+result_list = []
+itm_before = user_list[0]
+for itm in user_list:
+    if itm > itm_before:
+        result_list.append(itm)
+    itm_before = itm
+print(result_list)
