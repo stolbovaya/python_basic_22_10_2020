e2dcee0a3ca0a7bd90211f6bc0b2b2052ce459dd
stolbovaya <stolbovaya@yahoo.com>
2020-11-14 00:29:44 +0300

    Optimization task 1

--- homeworks/les7/task1.py
+++ homeworks/les7/task1.py
@@ -12,50 +12,49 @@
 
 
 class Matrix:
-    matrx = []
 
-    def __init__(self, matrx):
-        self.matrx = matrx
+    def __init__(self, array):
+        self.array = array
 
     def __str__(self):
         result = ''
 
-        for el in self.matrx:
+        for el in self.array:
             result += " ".join(map(str, el)) + "\n"
         return result
 
     def __add__(self, other):
-        cnt_r_1 = len(self.matrx)
-        cnt_r_2 = len(other.matrx)
+        cnt_r_1 = len(self.array)
+        cnt_r_2 = len(other.array)
         idx_row = 0
         result_row = []
         while idx_row < min(cnt_r_1, cnt_r_2):
-            cnt_c_1 = len(self.matrx[idx_row])
-            cnt_c_2 = len(other.matrx[idx_row])
+            cnt_c_1 = len(self.array[idx_row])
+            cnt_c_2 = len(other.array[idx_row])
             idx_col = 0
             result_col = []
             while idx_col < min(cnt_c_1, cnt_c_2):
-                result_col.append(self.matrx[idx_row][idx_col] + other.matrx[idx_row][idx_col])
+                result_col.append(self.array[idx_row][idx_col] + other.array[idx_row][idx_col])
                 idx_col += 1
             while idx_col < max(cnt_c_1, cnt_c_2):
                 if cnt_c_1 > cnt_c_2:
-                    result_col.append(self.matrx[idx_row][idx_col])
+                    result_col.append(self.array[idx_row][idx_col])
                 elif cnt_c_1 < cnt_c_2:
-                    result_col.append(other.matrx[idx_row][idx_col])
+                    result_col.append(other.array[idx_row][idx_col])
                 idx_col += 1
             result_row.append(result_col)
             idx_row += 1
         while idx_row < max(cnt_r_1, cnt_r_2):
             if cnt_r_1 > cnt_r_2:
-                result_row.append(self.matrx[idx_row])
+                result_row.append(self.array[idx_row])
             elif cnt_r_1 < cnt_r_2:
-                result_row.append(other.matrx[idx_row])
+                result_row.append(other.array[idx_row])
             idx_row += 1
 
-        return result_row
+        return Matrix(result_row)
 
 
 matrix1 = Matrix([[1, 2], [2, 2], [3, 3]])
 matrix2 = Matrix([[1, 1, 1], [2], [3, 3, 4, 5, 6], [7, 7]])
-matrix = Matrix(matrix1 + matrix2)
+matrix = matrix1 + matrix2
 print(matrix)
