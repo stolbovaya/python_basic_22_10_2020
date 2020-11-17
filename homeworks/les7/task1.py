68fda311c3da037daf8ebd201be4090249b8c10c
stolbovaya <stolbovaya@yahoo.com>
2020-11-17 23:17:53 +0300

    Optimization task 1

--- homeworks/les7/task1.py
+++ homeworks/les7/task1.py
@@ -9,52 +9,61 @@
 Подсказка: сложение элементов матриц выполнять поэлементно — первый элемент первой строки первой матрицы
 складываем с первым элементом первой строки второй матрицы и т.д.
 """
+from copy import deepcopy
 
 
 class Matrix:
 
-    def __init__(self, array):
-        self.array = array
+    def __init__(self, array: list):
+        self.__array = deepcopy(array)
+        self.__shape = (len(max(self.__array, key=len)), len(self.__array))
+        if len(min(self.__array, key=len)) != self.shape[0]:
+            self.__reshape()
 
-    def __str__(self):
-        result = ''
+    @property
+    def shape(self):
+        return self.__shape
 
-        for el in self.array:
-            result += " ".join(map(str, el)) + "\n"
-        return result
+    def __reshape(self):
+        for itm in self.__array:
+            tmp = self.shape[0] - len(itm)
+            if tmp:
+                itm.extend([0 for _ in range(tmp)])
+
+    def __getitem__(self, item):
+        return tuple(self.__array[item])
+
+    def __iter__(self):
+        return self.__array.__iter__()
 
     def __add__(self, other):
-        cnt_r_1 = len(self.array)
-        cnt_r_2 = len(other.array)
-        idx_row = 0
-        result_row = []
-        while idx_row < min(cnt_r_1, cnt_r_2):
-            cnt_c_1 = len(self.array[idx_row])
-            cnt_c_2 = len(other.array[idx_row])
-            idx_col = 0
-            result_col = []
-            while idx_col < min(cnt_c_1, cnt_c_2):
-                result_col.append(self.array[idx_row][idx_col] + other.array[idx_row][idx_col])
-                idx_col += 1
-            while idx_col < max(cnt_c_1, cnt_c_2):
-                if cnt_c_1 > cnt_c_2:
-                    result_col.append(self.array[idx_row][idx_col])
-                elif cnt_c_1 < cnt_c_2:
-                    result_col.append(other.array[idx_row][idx_col])
-                idx_col += 1
-            result_row.append(result_col)
-            idx_row += 1
-        while idx_row < max(cnt_r_1, cnt_r_2):
-            if cnt_r_1 > cnt_r_2:
-                result_row.append(self.array[idx_row])
-            elif cnt_r_1 < cnt_r_2:
-                result_row.append(other.array[idx_row])
-            idx_row += 1
+        if not isinstance(other, Matrix):
+            raise TypeError(f'This is Not Matrix this is {type(other).__name__}')
+        if self.shape != other.shape:
+            raise ValueError(f'Not equal shape matrix {self.shape} and {other.shape}')
 
-        return Matrix(result_row)
+        return Matrix(list(map(lambda y: list(map(sum, y)),
+                               map(lambda x: list(zip(*x)), zip(self, other))
+                               )
+                           )
+                      )
+
+    def __str__(self):
+
+        max_len_itm = len(str(max(map(lambda item: max(item, key=lambda x: len(str(x))), self.__array)))) + 1
+        if not max_len_itm & 1:
+            max_len_itm += 1
+
+        result = ''
+        for line in self.__array:
+            result += ''.join([f'{itm:^{max_len_itm}}' for itm in line]) + '\n'
+        return result
 
 
-matrix1 = Matrix([[1, 2], [2, 2], [3, 3]])
-matrix2 = Matrix([[1, 1, 1], [2], [3, 3, 4, 5, 6], [7, 7]])
-matrix = matrix1 + matrix2
-print(matrix)
+if __name__ == '__main__':
+    matrix1 = Matrix([[1, 2, 1], [2, 2], [3, 3]])
+    print(matrix1)
+    matrix2 = Matrix([[10, 1, 1], [2], [3, 3, 4]])
+    print(matrix2)
+    matrix = matrix1 + matrix2
+    print(matrix)
